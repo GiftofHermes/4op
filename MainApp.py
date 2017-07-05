a9a5a59b1538d1290614f1961114b6135dfd5de4
roohat <rohat.yildiz@outlook.com>
2017-07-05 20:39:15 +0300

    Still fooling with the kivy

--- MainApp.py
+++ MainApp.py
@@ -2,11 +2,16 @@ import kivy
 kivy.require("1.10.0") # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, ObjectProperty
 import GoalCreation
 from kivy.core.window import Window
 Window.size = (400, 400)
-import GoalCheck
+
+from kivy.uix.button import Button
+
+class MyButton(Button):
+    def on_num_pressed(self):
+        self.disabled = True
 
 class MainApp(App):
     numbers = GoalCreation.goal_creation(1,5)
@@ -17,6 +22,14 @@ class MainApp(App):
     num4 = NumericProperty(numbers[4])
     num5 = NumericProperty(numbers[5])
     num6 = NumericProperty(numbers[6])
+
+    selected_num = 0
+
+    def on_num_pressed(self, id):
+        print("number %d pressed" % id)
+
+    def on_op_pressed(self, id):
+        print("op %s pressed" % id)
 
 if __name__ == '__main__':
     MainApp().run()
